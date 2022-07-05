3c05a6a1ba8024f0f12998aaa3967d9a3556d279
Tom Worrall <hello@tcfw.com.au>
2022-07-06 08:18:30 +1000

    dns lib

--- pkg/did/resolver/dns.go
+++ pkg/did/resolver/dns.go
@@ -4,9 +4,20 @@ import (
 	"context"
 	"errors"
 
+	"github.com/miekg/dns"
 	"github.com/tcfw/didem/pkg/did/w3cdid"
 )
 
 func (r *Resolver) resolveDNS(ctx context.Context, did w3cdid.URL) (*w3cdid.Document, error) {
 	return nil, errors.New("not implemented")
+}
+
+type DNSDIDResolver struct {
+	dnsc *dns.Client
+}
+
+func NewDNSDIDResolver() *DNSDIDResolver {
+	dnsc := new(dns.Client)
+
+	return &DNSDIDResolver{dnsc}
 }
