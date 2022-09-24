b52882f8905b90fed6afe6243ae9070a3572223f
Tom Worrall <hello@tcfw.com.au>
2022-09-25 00:21:08 +1000

    genesis to support multiple nodes

--- scripts/gengen/gen.go
+++ scripts/gengen/gen.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/user"
-	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/interface-go-ipfs-core/options"
 	"github.com/ipfs/kubo/config"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multibase"
 	istorage "github.com/tcfw/didem/internal/storage"
 	"github.com/tcfw/didem/pkg/cryptography"
@@ -25,8 +25,167 @@ import (
 )
 
 var (
-	p2pIDfile = flag.String("id file", "~/.didem/p2p_identity", "p2p identiy file for nodes")
+	p2pIdents = flag.String("p2p_identities", "", "p2p identites for each node")
 )
+
+type identity struct {
+	blsSk   *cryptography.Bls12381PrivateKey
+	blsPKMB string
+	pID     peer.ID
+}
+
+func main() {
+	flag.Parse()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	config := &storage.GenesisInfo{
+		ChainID: "testnet",
+	}
+
+	idents := strings.Split(*p2pIdents, ",")
+
+	if len(idents) == 0 {
+		panic("no identities provided")
+	}
+
+	identities := []identity{}
+
+	for _, i := range idents {
+		sk, err := did.GenerateBls12381G2Identity()
+		if err != nil {
+			panic(err)
+		}
+
+		pk, err := sk.PublicIdentity()
+		if err != nil {
+			panic(err)
+		}
+
+		pkmb, err := pk.PublicKeys[0].AsMultibase()
+		if err != nil {
+			panic(err)
+		}
+
+		pid := peer.ID(i)
+
+		identities = append(identities, identity{
+			blsSk:   sk.PrivateKey().(*cryptography.Bls12381PrivateKey),
+			blsPKMB: pkmb,
+			pID:     pid,
+		})
+	}
+
+	txs := []*tx.Tx{}
+
+	for _, i := range identities {
+		txs = append(txs,
+			&tx.Tx{
+				Version: tx.Version1,
+				Ts:      time.Now().Unix(),
+				From:    i.pID.Pretty(),
+				Type:    tx.TxType_DID,
+				Action:  tx.TxActionAdd,
+				Data: &tx.DID{
+					Document: &w3cdid.Document{
+						ID: "did:didem:" + i.pID.String(),
+						Authentication: []cryptography.VerificationMethod{
+							{
+								ID:                 "did:didem:" + i.pID.String(),
+								Type:               cryptography.Bls12381G2Key2020,
+								PublicKeyMultibase: i.blsPKMB,
+							},
+						},
+					},
+				},
+			},
+			&tx.Tx{
+				Version: tx.Version1,
+				Ts:      time.Now().Unix(),
+				From:    i.pID.String(),
+				Type:    tx.TxType_Node,
+				Action:  tx.TxActionAdd,
+				Data: &tx.Node{
+					Id:  i.pID.String(),
+					Did: "did:didem:" + i.pID.String(),
+					Key: []byte(i.blsPKMB),
+				},
+			},
+		)
+	}
+
+	txCids := []cid.Cid{}
+
+	store, close, err := tempIPFSStorage(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	defer close()
+
+	for _, t := range txs {
+		msg, err := t.Marshal()
+		if err != nil {
+			panic(err)
+		}
+		t.Signature, err = identities[0].blsSk.Sign(nil, msg, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		cid, err := store.PutTx(ctx, t)
+		if err != nil {
+			panic(err)
+		}
+		txCids = append(txCids, cid)
+	}
+
+	block, err := storage.NewBlock(ctx, store, storage.BlockID(cid.Undef), txCids)
+	if err != nil {
+		panic(err)
+	}
+
+	blMsg, err := msgpack.Marshal(block)
+	if err != nil {
+		panic(err)
+	}
+	block.Signature, err = identities[0].blsSk.Sign(nil, blMsg, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	config.Block = *block
+	config.Txs = txs
+
+	b, err := msgpack.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+
+	b64, err := multibase.Encode(multibase.Base58BTC, b)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Genesis Config:\n%s", b64)
+	fmt.Printf("\n\nGenesis Config (Hex):\n%s", hex.EncodeToString(b))
+
+	fmt.Printf("\n\nIdentities: \n")
+	for i, id := range identities {
+		skb, err := id.blsSk.Bytes()
+		if err != nil {
+			panic(err)
+		}
+
+		skbmb, err := multibase.Encode(multibase.Base58BTC, skb)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Identity %d\n\tSK: %s\n\tID: %s\n\tPeer: %s\n\n", i, skbmb, id.pID.Pretty(), idents[i])
+	}
+
+}
 
 func tempIPFSStorage(ctx context.Context) (*istorage.IPFSStorage, func(), error) {
 	repoPath, err := ioutil.TempDir("", "ipfs-shell")
@@ -50,162 +209,4 @@ func tempIPFSStorage(ctx context.Context) (*istorage.IPFSStorage, func(), error)
 	}
 
 	return ipfs, c, nil
-}
-
-func main() {
-	flag.Parse()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	defer cancel()
-
-	// p2pidFile := expandPath(*p2pIDfile)
-
-	// idB, err := ioutil.ReadFile(p2pidFile)
-	// if err != nil {
-	// 	panic(errors.Wrap(err, "reading identity file"))
-	// }
-
-	// p2ppriv, err := crypto.UnmarshalPrivateKey(idB)
-	// if err != nil {
-	// 	panic(errors.Wrap(err, "unmarshaling private key"))
-	// }
-
-	// nid, err := peer.IDFromPrivateKey(p2ppriv)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	config := &storage.GenesisInfo{
-		ChainID: "testnet",
-	}
-
-	sk, err := did.GenerateBls12381G2Identity()
-	if err != nil {
-		panic(err)
-	}
-	blsSk := sk.PrivateKey().(*cryptography.Bls12381PrivateKey)
-
-	pk, err := sk.PublicIdentity()
-	if err != nil {
-		panic(err)
-	}
-
-	pkmb, err := pk.PublicKeys[0].AsMultibase()
-	if err != nil {
-		panic(err)
-	}
-
-	txs := []*tx.Tx{
-		{
-			Version: tx.Version1,
-			Ts:      time.Now().Unix(),
-			From:    pk.ID,
-			Type:    tx.TxType_DID,
-			Action:  tx.TxActionAdd,
-			Data: &tx.DID{
-				Document: &w3cdid.Document{
-					ID: "did:didem:" + pk.ID,
-					Authentication: []cryptography.VerificationMethod{
-						{
-							ID:                 "did:didem:" + pk.ID,
-							Type:               cryptography.Bls12381G2Key2020,
-							PublicKeyMultibase: pkmb,
-						},
-					},
-				},
-			},
-		},
-		{
-			Version: tx.Version1,
-			Ts:      time.Now().Unix(),
-			From:    pk.ID,
-			Type:    tx.TxType_Node,
-			Action:  tx.TxActionAdd,
-			Data: &tx.Node{
-				Id:  "12D3KooWHovX9xtxzV9SkRJUrHZbYHUMCfpiHJgCR2QzULtfywf3",
-				Did: "did:didem:" + pk.ID,
-				Key: []byte(pkmb),
-			},
-		},
-	}
-	txCids := []cid.Cid{}
-
-	store, close, err := tempIPFSStorage(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	defer close()
-
-	for _, t := range txs {
-		msg, err := t.Marshal()
-		if err != nil {
-			panic(err)
-		}
-		t.Signature, err = blsSk.Sign(nil, msg, nil)
-		if err != nil {
-			panic(err)
-		}
-
-		cid, err := store.PutTx(ctx, t)
-		if err != nil {
-			panic(err)
-		}
-		txCids = append(txCids, cid)
-	}
-
-	block, err := storage.NewBlock(ctx, store, storage.BlockID(cid.Undef), txCids)
-	if err != nil {
-		panic(err)
-	}
-
-	blMsg, err := msgpack.Marshal(block)
-	if err != nil {
-		panic(err)
-	}
-	block.Signature, err = blsSk.Sign(nil, blMsg, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	config.Block = *block
-	config.Txs = txs
-
-	b, err := msgpack.Marshal(config)
-	if err != nil {
-		panic(err)
-	}
-
-	b64, err := multibase.Encode(multibase.Base58BTC, b)
-	if err != nil {
-		panic(err)
-	}
-
-	blsSkB, err := blsSk.Bytes()
-	if err != nil {
-		panic(err)
-	}
-
-	s, err := multibase.Encode(multibase.Base58BTC, blsSkB)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("SK: %s\n", s)
-
-	fmt.Printf("Genesis Config:\n%s", b64)
-}
-
-func expandPath(path string) string {
-	usr, _ := user.Current()
-	dir := usr.HomeDir
-	if path == "~" {
-		// In case of "~", which won't be caught by the "else if"
-		path = dir
-	} else if strings.HasPrefix(path, "~/") {
-		// Use strings.HasPrefix so we don't match paths like
-		// "/something/~/something/"
-		path = filepath.Join(dir, path[2:])
-	}
-
-	return path
 }
